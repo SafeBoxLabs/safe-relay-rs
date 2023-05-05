f6dd3c39323e83082cebc9e8e163aeb6b349443c
chainchampion <suryansh.dev@proton.me>
2023-05-05 16:06:58 +0530

    Register Safe management endpoint services

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::env;
-use actix_web::{App, HttpServer};
-use actix_web::middleware;
+use std::sync::Arc;
+
+use actix_cors::Cors;
+use actix_web::{App, middleware};
+use actix_web::HttpServer;
 use actix_web::middleware::Logger;
+use actix_web::web;
 use dotenv::dotenv;
-use env_logger;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
 
 use crate::safe::{SafeInfo, SafeResponse};
 use crate::safe_config::SafeConfig;
@@ -20,6 +25,7 @@ pub(crate) mod ethers_ext;
 
 #[derive(OpenApi)]
 #[
+
 openapi(paths(calculate_address, deploy_contract, exec_transaction),
 components(schemas(SafeInfo, SafeCall, SafeResponse, SafeErr)),
 tags(
@@ -36,6 +42,9 @@ async fn main() -> std::io::Result<()> {
     env::set_var("RUST_BACKTRACE", "full");
 
     env_logger::init_from_env(env_logger::Env::new());
+    let safe_use_case = SafeUseCase::new(
+        Arc::new(SafeService::new(SafeConfig::new()).await)
+    );
 
     let address = env::var("ADDRESS")
         .expect("ADDRESS must be defined");
@@ -57,6 +66,14 @@ async fn main() -> std::io::Result<()> {
                     .supports_credentials()
                     .max_age(3600),
             )
+            .app_data(web::Data::new(safe_use_case.clone()))
+            .service(
+                SwaggerUi::new("/swagger/{_:.*}")
+                    .url("/api-doc/openapi.json", ApiDoc::openapi()),
+            )
+            .service(calculate_address)
+            .service(deploy_contract)
+            .service(exec_transaction)
     })
         .bind((address, port))?
         .run()
